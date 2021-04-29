b819c95e0e4f6291241e0d3403e99c04ef8ffd8d
theheroproject <thenaheroproject@gmail.com>
2021-04-29 15:05:45 +0900

    my first commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.example.restfulwebservice;
+// lombok
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+    // lombok 사용시 get,set,생성자가 필요없다.
+/*    public String getMessage(){
+        return this.message;
+    }
+    public void setmessage(){
+        this.message = msg;
+    }*/
+    // 생성자 작성 불필요
+//    public HelloWorldBean(String message) {
+//         this.message=message;
+//    }
+
+}
