d8c1bbe27adf70f8356b62ea775ebb1e9006fb2c
theheroproject <thenaheroproject@gmail.com>
2021-05-12 23:32:46 +0900

    my first commit

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.example.restfulwebservice.helloworld;
 
 
+import com.example.restfulwebservice.user.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     // GET
     // /hello-world (endpoint)
     // @RequestMapping(method-RequestMethod.GET , path="/hello-world")
@@ -24,4 +33,8 @@ public class HelloWorldController {
         return new HelloWorldBean(String.format("Hello World, %s®" , name));
     }
 
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language",required = false) Locale locale){
+        return messageSource.getMessage("greeting.message",null,locale);
+    }
 }
