eb74c18dd4595df9cf146e403f50c9ee7bea1874
theheroproject <thenaheroproject@gmail.com>
2021-05-14 22:50:03 +0900

    my first commit

---
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -0,0 +1,36 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService service;
+
+    public AdminUserController(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveAllUser(@PathVariable int id){
+        User user = service.findOne(id);
+
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s]not found", id));
+        }
+        return user;
+    }
+
+}
