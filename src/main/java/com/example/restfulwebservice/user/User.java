58fc94a73e5ac371f92d918244070241fdcce3b0
theheroproject <thenaheroproject@gmail.com>
2021-06-03 01:32:02 +0900

    my first commit

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,19 +17,25 @@ import java.util.Date;
 @AllArgsConstructor
 //@JsonIgnoreProperties(value={"password"})
 //@JsonFilter("UserInfo")
+
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 @NoArgsConstructor
 public class User {
     private Integer id;
 
     @Size(min=2,message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
     //lombok 사용
 
     //외부에 노출시키고 싶지 않은 값에 @JsonIgnore 을 추가한다.
     //@JsonIgnore
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요.")
     private String password;
     //@JsonIgnore
+    @ApiModelProperty(notes = "사용자 주민등록번호를 입력해 주세요.")
     private String ssn;
 }
